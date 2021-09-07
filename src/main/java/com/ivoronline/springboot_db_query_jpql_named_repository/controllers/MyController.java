cf205eba9560b8b6c4e6d0fed41660a8981ac724
igoreta <ivoronline@gmail.com>
2021-09-07 15:44:03 +0200

    Commit

--- src/main/java/com/ivoronline/springboot_db_query_jpql_named_repository/controllers/MyController.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql_named_repository/controllers/MyController.java
@@ -2,7 +2,6 @@ package com.ivoronline.springboot_db_query_jpql_named_repository.controllers;
 
 import com.ivoronline.springboot_db_query_jpql_named_repository.entities.Person;
 import com.ivoronline.springboot_db_query_jpql_named_repository.repositories.PersonRepository;
-import com.ivoronline.springboot_db_query_jpql_named_repository.services.DBAccess;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyController {
 
-  @Autowired DBAccess         dbAccess;
   @Autowired PersonRepository personRepository;    //Only for INSERT
 
   //================================================================
-  // SELECT PERSON BY NAME AGE
+  // SELECT PERSON
   //================================================================
-  @RequestMapping("SelectPersonByNameAge")
-  Person selectPersonByNameAge() {
-    Person person = dbAccess.selectPersonByNameAge();
+  @RequestMapping("SelectPerson")
+  Person selectPerson() {
+    Person person = personRepository.selectPerson("John", 20);
     return person;
   }
 
@@ -27,7 +25,7 @@ public class MyController {
   //================================================================
   @RequestMapping("InsertPerson")
   String insertPerson() {
-    dbAccess.insertPerson();
+    personRepository.save(new Person("John" , 20));
     return "Person Inserted into DB";
   }
   //================================================================
@@ -35,7 +33,7 @@ public class MyController {
   //================================================================
   @RequestMapping("UpdatePerson")
   String updatePerson() {
-    Integer updatedRecords = dbAccess.updatePerson();
+    Integer updatedRecords = personRepository.updatePerson("John", 200);
     return  updatedRecords + " Records Updated";
   }
 
@@ -44,7 +42,7 @@ public class MyController {
   //================================================================
   @RequestMapping("DeletePerson")
   String deletePerson() {
-    Integer deletedRecords = dbAccess.deletePerson();
+    Integer deletedRecords = personRepository.deletePerson("John");
     return  deletedRecords + " Records Deleted";
   }
 
--- src/main/java/com/ivoronline/springboot_db_query_jpql_named_repository/repositories/PersonRepository.java
+++ src/main/java/com/ivoronline/springboot_db_query_jpql_named_repository/repositories/PersonRepository.java
@@ -3,11 +3,12 @@ package com.ivoronline.springboot_db_query_jpql_named_repository.repositories;
 import com.ivoronline.springboot_db_query_jpql_named_repository.entities.Person;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface PersonRepository extends CrudRepository<Person, Integer> {
-             Person  selectPersonByNameAge(String name, Integer age   );
-  @Modifying Integer updatePerson         (String name, Integer newAge);
-  @Modifying Integer deletePerson         (String name                );
+                            Person  selectPerson(String name, Integer age   );
+  @Transactional @Modifying Integer updatePerson(String name, Integer newAge);
+  @Transactional @Modifying Integer deletePerson(String name                );
 }
 
 
